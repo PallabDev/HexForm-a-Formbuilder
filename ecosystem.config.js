62cfdf03b783b78dd8ba82aaf5e637d443abc7e0
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-05-27 01:27:07 +0530

    feat: update deployment configuration to use PNPM and streamline deployment steps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  apps: [
-    {
-      name: "hexform-backend",
-      cwd: "./apps/api",
-      script: "npm",
-      args: "run start",
-      env: {
-        PORT: 8000,
-        NODE_ENV: "production",
-      },
-    },
-    {
-      name: "hexform-frontend",
-      cwd: "./apps/web",
-      script: "npm",
-      args: "run start",
-      env: {
-        PORT: 3000,
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "frontend",
+            cwd: "/root/projects/HexForm-a-Formbuilder",
+            script: "pnpm",
+            args: "--filter web start",
+            env: {
+                NODE_ENV: "production",
+                PORT: 5600,
+            },
+        },
+        {
+            name: "backend",
+            cwd: "/root/projects/HexForm-a-Formbuilder",
+            script: "pnpm",
+            args: "--filter @repo/api start",
+            env: {
+                NODE_ENV: "production",
+                PORT: 8600,
+            },
+        },
+    ],
 };
